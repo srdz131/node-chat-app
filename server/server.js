43f78af93cc010b0fbb1551da3f209655ac5ca40
srdz131 <shrvacanin@gmail.com>
2017-05-24 17:39:04 +0200

    Init commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const publicPath =  path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath))
+
+app.listen(3000);
+console.log(`You are on port ${port}`);
