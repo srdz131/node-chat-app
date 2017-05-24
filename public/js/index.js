ce5f233904643eb239451882a60d96b9c67072c8
srdz131 <shrvacanin@gmail.com>
2017-05-24 21:54:26 +0200

    Added geolocation option

--- public/js/index.js
+++ public/js/index.js
@@ -24,4 +24,17 @@ jQuery('#message-form').on('submit', function(e){
     from: 'User',
     text: jQuery('[name=message]').val()
   });
+});
+
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(){
+  if(!navigator.geolocation){
+    return alert('You dont have geolocation supported');
+  }
+  navigator.geolocation.getCurrentPosition(function(position){
+    console.log(position);
+  }, function(){
+    alert('Unable to fetch location');
+  })
 })
