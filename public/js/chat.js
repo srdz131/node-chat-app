5eb5911db87c7b0e8ca8d407817b9b4fbdd557ac
srdz131 <shrvacanin@gmail.com>
2017-05-26 20:31:28 +0200

    Added html login page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom(){
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //heights
+  //var containerHeight = jQuery('.flex-container');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollHeight + newMessageHeight + lastMessageHeight>=scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', function(){
   console.log('connected to the server')
 });
@@ -14,6 +31,7 @@ socket.on('newMessage', function(message){
   });
 
   jQuery('#messages').append(html)
+  scrollToBottom();
   // console.log('new message recived:',message);
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formatedTime}: ${message.text}`);
